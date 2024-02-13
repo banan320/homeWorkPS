86934bdde6aee28984de767814a36ec785fa3e13
Banan320 <alex-gerber@mail.ru>
2024-02-13 18:16:13 +0300

    навигатор

---
+++ 4-navigator/app.js
@@ -0,0 +1,8 @@
+const addressLat = 100;
+const addressLong = 500;
+const positionLat = 10;
+const positionLong = 500;
+
+let res =
+  ((addressLat - positionLat) ** 2 + (addressLong - positionLong) ** 2) ** 0.5;
+console.log(res);
