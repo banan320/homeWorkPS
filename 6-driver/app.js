24e6c127f817ca89ffd522bdd7c8db96c60f99d6
Banan320 <alex-gerber@mail.ru>
2024-02-14 17:50:35 +0300

    ревью переменной

--- 6-driver/app.js
+++ 6-driver/app.js
@@ -3,5 +3,5 @@ const hasLicense = true;
 const age = 27;
 const isDrunk = true;
 
-let res = age > 18 && hasLicense && !isDrunk ? "может" : "не может";
+let canDrive = age > 18 && hasLicense && !isDrunk ? "может" : "не может";
 console.log(`Водитель ${res} сесть за руль`);
