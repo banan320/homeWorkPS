56e52b97b96e7bd1eb85188fd6451361a17a5334
Banan320 <alex-gerber@mail.ru>
2024-02-14 17:57:57 +0300

    ревью вывода сообщения

--- 6-driver/app.js
+++ 6-driver/app.js
@@ -4,4 +4,4 @@ const age = 27;
 const isDrunk = true;
 
 let canDrive = age > 18 && hasLicense && !isDrunk ? "может" : "не может";
-console.log(`Водитель ${res} сесть за руль`);
+console.log(`Водитель ${canDrive} сесть за руль`);
