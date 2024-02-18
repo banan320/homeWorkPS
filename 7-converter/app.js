88563002c968367e2605dab8158380f5020d6c89
Banan320 <alex-gerber@mail.ru>
2024-02-18 21:14:46 +0300

    конвертация валют

---
+++ 7-converter/app.js
@@ -0,0 +1,28 @@
+// Конвертация валют ============
+function currencyConversion(summ, currency = "rub", targetCurrency = "usd") {
+  let rubToUsd = 0.011;
+  let rubToEur = 0.01;
+  let rubToCny = 0.077;
+  let dollarToRub = 90;
+  let eurToRub = 97;
+  let cnyToRub = 12;
+
+  switch (true) {
+    case targetCurrency === "usd" && currency === "rub":
+      return summ * rubToUsd;
+    case targetCurrency === "eur" && currency === "rub":
+      return summ * rubToEur;
+    case targetCurrency === "cny" && currency === "rub":
+      return summ * rubToCny;
+
+    case targetCurrency === "rub" && currency === "usd":
+      return summ * dollarToRub;
+    case targetCurrency === "rub" && currency === "eur":
+      return summ * eurToRub;
+    case targetCurrency === "rub" && currency === "cny":
+      return summ * cnyToRub;
+    default:
+      return null;
+  }
+}
+console.log(currencyConversion(10, "usd", "rub"));
